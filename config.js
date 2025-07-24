ace2ae0aba2eafa171387b2db56d8706a9e85578
satyabharathi19 <148929278+satyabharathi19@users.noreply.github.com>
2025-07-24 13:44:08 +0530

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = process.env.PORT || 8000;
-
-export const mongoURL = "mongodb+srv://amirchoudharyb09:Am%4017092003@cluster0.j07nabs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
